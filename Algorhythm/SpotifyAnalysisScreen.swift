ad8f0d3be5415011429a34131de5d8afbe74983a
Jack Belding <jack_belding@yahoo.com>
2022-11-12 12:19:03 -0500

    Add get top tracks API call

--- Algorhythm/SpotifyAnalysisScreen.swift
+++ Algorhythm/SpotifyAnalysisScreen.swift
@@ -1,29 +1,49 @@
 //
 //  SpotifyAnalysisScreen.swift
-//  XcodersGraphQL
 //
 //  Created by Jack Belding on 10/12/22.
 //
 
 import Foundation
 import SwiftUI
+import Combine
+import SpotifyWebAPI
+import SpotifyExampleContent
 
 struct SpotifyAnalysisScreen: View{
-    
+    // spotify object
+    @EnvironmentObject var spotify: Spotify
+
+    // View model for mood analysis and data storage
     @StateObject private var analyzedSongListVM = SpotifyAnalysisListViewModel()
     
+    // display loading page
+    @State private var isLoadingPage = false
+    
+    // the array of user top tracks to be filtered for moods
+    @State private var userTopTracks: [Track]
+
+    // cancellable for api
+    @State private var getUserTopTracksCancellable: AnyCancellable? = nil
+
+    // store an alert
+    @State private var alert: AlertItem? = nil
+
+    
+    // initializer
+    init() {
+        self._userTopTracks = State(initialValue: [])
+    }
+    // preview initializer
+    fileprivate init(topTracks: [Track]) {
+        self._userTopTracks = State(initialValue: topTracks)
+    }
+    
     var body: some View {
-//        VStack {
-//            Image(systemName: "globe")
-//                .imageScale(.large)
-//                .foregroundColor(.accentColor)
-//            Text("Hello, world!")
-//        }
         NavigationView{
             VStack{
                 List(analyzedSongListVM.analyzedSongs, id: \.id){song in
                     Text(song.id)
-    //                Button("Stats")
                 }
                 Spacer()
                 Button(action: {analyzedSongListVM.printNetworkCalls()}){
@@ -33,9 +53,62 @@ struct SpotifyAnalysisScreen: View{
 
         }
         .onAppear(perform: {
-            analyzedSongListVM.populateRecentlyPlayedSongAnalysis()
+            getTopTracks(trackLimit: 25)
+//            analyzedSongListVM.populateRecentlyPlayedSongAnalysis()
         })
         .navigationTitle("Recently played songs")
         .padding()
+    }
+    /**
+     * function returns user top tracks
+     */
+    func getTopTracks(trackLimit:Int?) {
+        self.isLoadingPage = true
+        self.userTopTracks = []
+        
+        self.getUserTopTracksCancellable = self.spotify.api
+            .currentUserTopTracks(limit:trackLimit)
+            .receive(on: RunLoop.main)
+            .sink(
+                receiveCompletion: self.getTopTracksCompletion(_:),
+                receiveValue: {
+                    response in self.userTopTracks = response.items
+                    print(
+                        "received first page with \(userTopTracks.count) items"
+                    )
+                    analyzedSongListVM.networkCalls.spotify += 1
+                })
+    }
+    
+    func getTopTracksCompletion(
+        _ completion: Subscribers.Completion<Error>
+    ) {
+        if case .failure(let error) = completion {
+            let title = "Couldn't retrieve user top tracks"
+            print("\(title): \(error)")
+            self.alert = AlertItem(
+                title: title,
+                message: error.localizedDescription
+            )
+        }
+        self.isLoadingPage = false
+    }
+}
+
+struct SpotifyAnalysisScreen_Previews: PreviewProvider {
+
+    static let tracks: [Track] = [
+        .because, .comeTogether, .faces, .illWind,
+        .odeToViceroy, .reckoner, .theEnd, .time
+    ]
+
+    static var previews: some View {
+        ForEach([tracks], id: \.self) { tracks in
+            NavigationView {
+                SpotifyAnalysisScreen(topTracks: tracks)
+                    .listStyle(PlainListStyle())
+            }
+        }
+        .environmentObject(Spotify())
     }
 }
--- Algorhythm/SpotifyAnalysisViewModel.swift
+++ Algorhythm/SpotifyAnalysisViewModel.swift
@@ -1,23 +1,45 @@
-//
-//  SpotifyAnalysisViewModel.swift
-//  XcodersGraphQL
-//
-//  Created by Jack Belding on 10/12/22.
-//
+/**
+ * SpotifyAnalysisViewModel.swift
+ * Created by Jack Belding on 10/12/22.
+ */
 
 import Foundation
 import Apollo
+import Combine
+import SpotifyWebAPI
+import SpotifyExampleContent
 
+/**
+ * object to keep count of network calls
+ */
 struct NetworkCalls{
     var spotify:Int = 0
     var cyanite:Int = 0
     var total = 0
 }
 
-
+/**
+ *  The list of all the analyzed spotify songs. This is only for observing the results of the analysis
+ * and will not be used in the production application
+ */
 class SpotifyAnalysisListViewModel: ObservableObject {
     // Network calls logger
-    private var networkCalls:NetworkCalls = NetworkCalls()
+    public var networkCalls:NetworkCalls = NetworkCalls()
+    
+    // the list of analyzed songs
+    @Published var analyzedSongs: [SpotifyAnalysisViewModel] = []
+    
+    // songs to analyze
+    let songIds = ["5uu28fUesZMl89lf9CLrgN", "7nwGvxqSrgAuty1tlPGCFz", "7iQM9DQUFKUSNjVt8GQZV2",
+    "4YHkUrSYE0xzUqp7noMUWD", "7KT7VGnPU5QVXN3q1BOeqb"]
+}
+
+/**
+ * utility functions
+ */
+extension SpotifyAnalysisListViewModel {
+    
+    // print the contents of the network call logger
     func printNetworkCalls() -> Void {
         print("""
               ###########################################\n
@@ -27,18 +49,21 @@ class SpotifyAnalysisListViewModel: ObservableObject {
               ###########################################\n
               """)
     }
-    
-    @Published var analyzedSongs: [SpotifyAnalysisViewModel] = []
-    let songIds = ["5uu28fUesZMl89lf9CLrgN", "7nwGvxqSrgAuty1tlPGCFz", "7iQM9DQUFKUSNjVt8GQZV2",
-    "4YHkUrSYE0xzUqp7noMUWD", "7KT7VGnPU5QVXN3q1BOeqb"]
-    
+}
+/**
+ * Filter seeds by mood
+ */
+extension SpotifyAnalysisListViewModel{
+    //
+    // request to cyanite API to analyze a list of tracks
+    //
     func populateRecentlyPlayedSongAnalysis() {
         // get track analysis
         for id in songIds{
-            Network.shared.apollo.fetch(query: SpotifyTrackQueryQuery(id: id	)){ [weak self] result in
+            Network.shared.apollo.fetch(query: SpotifyTrackQueryQuery(id: id    )){ [weak self] result in
                 switch result {
                 case .success(let graphQLResult):
-                   if let analyzedSongs = graphQLResult.data?.spotifyTrack {
+                    if let analyzedSongs = graphQLResult.data?.spotifyTrack {
                         DispatchQueue.main.async {
                             self?.analyzedSongs.append(SpotifyAnalysisViewModel.init(analyzedSpotifyTrack: analyzedSongs))
                             self?.networkCalls.cyanite += 1
@@ -50,11 +75,13 @@ class SpotifyAnalysisListViewModel: ObservableObject {
                 }
             }
         }
-        
-
     }
 }
 
+/**
+ *  This represents an analyzed spotify song to be used as a seed. This will be
+ *  used for each generation, and will potentially be saved into the mood cache
+ */
 class SpotifyAnalysisViewModel {
     
     let analyzedSpotifyTrack: SpotifyTrackQueryQuery.Data.SpotifyTrack
